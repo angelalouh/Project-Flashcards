272b3eff2158598354834f0e77fb717a05bb4244
Angela Louh <louh.angela@gmail.com>
2022-03-30 18:58:51 -0700

    Added <hr/> between card text and card buttons; changed edit button color to dark; converted class to className

--- src/Layout/decks/DeckScreenCard.js
+++ src/Layout/decks/DeckScreenCard.js
@@ -5,8 +5,8 @@ import { deleteCard } from "../../utils/api/index";
 function DeckScreenCard({ cards, deckId, url }) {
   const history = useHistory();
 
-  // When the user clicks the "Delete" button associated with a card, this warning message will show.
-  // If the user clicks "OK", the card is deleted using deleteCard()
+  /* When the user clicks the "Delete" button associated with a card, this warning message will show.
+  If the user clicks "OK", the card is deleted using deleteCard() */
   const handleDeleteCardClick = (card) => {
     if (
       window.confirm("Delete this card? You will not be able to recover it.")
@@ -18,36 +18,40 @@ function DeckScreenCard({ cards, deckId, url }) {
   // Creates a Bootstrap card for each card in the deck with an edit and delete button
   const cardDisplay = cards.map((card, index) => {
     return (
-      <div class="card" key={index}>
-        <div class="card-body row">
-          <div class="col-md-5">
-            <p class="card-text">{card.front}</p>
+      <div className="deck-card card mt-2" key={index}>
+        <div className="card-body row">
+          <div className="col-md-5 pl-3">
+            <p className="font-weight-bold">Front:</p>
+            <p className="card-text ">{card.front}</p>
           </div>
-          <div class="col-md-5 ml-auto">
-            <p class="card-text">{card.back}</p>
+          <div className="col-md-5 ml-auto">
+            <p className="font-weight-bold">Back:</p>
+            <p className="card-text">{card.back}</p>
           </div>
         </div>
 
-        <div class="ml-auto mt-2">
+        <hr />
+        
+        <div className="ml-auto mt-2">
           <button
             type="button"
-            class="btn btn-secondary mr-2 mb-2"
+            className="btn btn-dark mr-2"
             onClick={() =>
               history.push(`/decks/${deckId}/cards/${card.id}/edit`)
             }
           >
-            <span class="oi oi-pencil" /> Edit
+            <span className="oi oi-pencil" /> Edit
           </button>
 
           <button
             type="button"
-            class="btn btn-danger mr-3 mb-2"
+            className="btn btn-danger"
             onClick={() => handleDeleteCardClick(card)}
           >
             {/* The anchor element will trigger the page to refresh after the card has been deleted.
             This makes it so that the refreshed page will no longer display the deleted card. */}
-            <a href={url} class="text-white">
-              <span class="oi oi-trash" />
+            <a href={url} className="text-white">
+              <span className="oi oi-trash" />
             </a>
           </button>
         </div>
