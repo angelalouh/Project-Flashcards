2289fc9fb73bb6c04a1712310533422a38d19e2f
Angela Louh <louh.angela@gmail.com>
2022-03-30 18:55:07 -0700

    Changed button color to dark; converted class to className

--- src/Layout/decks-study/FlipButton.js
+++ src/Layout/decks-study/FlipButton.js
@@ -6,7 +6,7 @@ function FlipButton({ setIsFrontOfCard }) {
   };
 
   return (
-    <button type="button" class="btn btn-secondary mr-2" onClick={flipCardHandler}>
+    <button type="button" className="btn btn-dark mr-2" onClick={flipCardHandler}>
       Flip
     </button>
   );
